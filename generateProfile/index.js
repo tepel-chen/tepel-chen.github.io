a958b635716ab05419a50219ce5946519153b4fd
tepel-chen <tepelchen501@gmail.com>
2022-07-12 05:03:39 +0900

    a

--- generateProfile/index.js
+++ generateProfile/index.js
@@ -51,7 +51,7 @@ const ignore = ["Jailbreak", "boneAppleTea", "boggle", "BrokenBinary", "Souvenir
     const date = new Date();
     const datestr = `${date.getFullYear()% 100}.${("0" + (new Date().getMonth()+1)).substr(-2,2)}.${("0" + new Date().getDate()).substr(-2,2)}`
     console.log(`* <a href="./Ja manual v1.${lastVersion + 1}.json" download>v1.${lastVersion + 1}</a> \\[最終更新: ${datestr}\\]`)
-    console.log(`|${datestr}|<a href="./Ja manual v1.${lastVersion + 1}.json" download>v1.${lastVersion + 1}</a>|{{ changelog v1.${lastVersion+1}.txt }}`);
+    console.log(`|${datestr}|<a href="./Ja manual v1.${lastVersion + 1}.json" download>v1.${lastVersion + 1}</a>|{% include_relative changelog${lastVersion + 1}<.txt %}|`);
     changed.forEach(c => console.log(`https://steamcommunity.com/sharedfiles/filedetails/?id=${names[c][2]}`));
     await fs.promises.writeFile(`${PROFILE_PATH}//changelog v1.${lastVersion+1}.txt`, changelog);
     await fs.promises.writeFile(`${PROFILE_PATH}//Ja manual v1.${lastVersion+1}.json`, JSON.stringify(result, null, 2));
